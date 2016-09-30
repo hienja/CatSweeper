f0f16059f918d34b52c0de7887a2ba41a50a4b9e
Hien Tran <aznketchup@yahoo.com>
2016-09-30 11:34:15 -0700

    Implement funtionalities for mousedown event.
    -Add poop image to assets.
    -Refactor css to be more DRY.
    -Add header and body class.
    -Insert header and body class into container.
    -Add functionality for right mouse click to add poop.
    -Add functionality for left mouse click to sink squares.

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ $(document).ready(function(){
     return false;
   };
 
-  $('.body').mousedown(function(e){ 
-      console.log(e.button)  
+  $('.body').mousedown(function(event){ 
+    console.log(event);
+    if (event.button === 2 && event.target.classList.contains('square') && !event.target.classList.contains('sink')) {
+      event.target.classList.toggle('poop');
+    }
+    if (event.button === 0 && event.target.classList.contains('square') && !event.target.classList.contains('poop')) {
+      event.target.classList.add('sink');
+    }
   }); 
 });
