81c5d8a58143f9cf3c5076af36196da7ed233f88
Hien Tran <aznketchup@yahoo.com>
2016-09-30 23:37:24 -0700

    Implement logic to handle what happen when pressing on a black square.
    -Implement recursive function that will continue to find adjacent blank squares when initally pressing on a blank square.
    -Use innerHTML to find value inside div tag.
    -Download mp3 of cat audio.
    -Play cat audio when finding a cat.
    -Display cat image when finding a cat.
    -Declare variable for winning, called safeCount.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var rows = 9;
 var columns = 9;
 var catsRemaining = 10;
 var catsLocation = {};
+var safeCount = rows * columns - catsRemaining;
 
 for (var i = 0; i < rows; i++) {
   var row = [];
@@ -68,14 +69,9 @@ for (var i = 0; i < rows; i++) {
 }
 
 $(document).ready(function(){
-  for (var key in catsLocation) {
-    for (var i = 0; i < catsLocation[key].length; i++) {
-      $('.' + Number(key) + ' :nth-child(' + catsLocation[key][i] + ')').addClass('poop');
-    }
-  }
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
-      if (board[i][j] !== 'cat') {
+      if (board[i][j] !== 0) {
         $('.' + (i + 1) + ' :nth-child(' + (j + 1) + ')').text(board[i][j]);
       }
     }
@@ -86,12 +82,62 @@ $(document).ready(function(){
     return false;
   };
 
-  $('.body').mousedown(function(event){ 
-    if (event.button === 2 && event.target.classList.contains('square') && !event.target.classList.contains('sink')) {
+  $('.body').mousedown(function(event){
+    var currentSquare = event.target;
+    var x = Number(currentSquare.classList[1]);
+    var y = Number(currentSquare.parentElement.classList[1]);
+
+    if (event.button === 2 && currentSquare.classList.contains('square') && !currentSquare.classList.contains('sink')) {
       event.target.classList.toggle('poop');
     }
-    if (event.button === 0 && event.target.classList.contains('square') && !event.target.classList.contains('poop')) {
-      event.target.classList.add('sink', 'show');
+    if (event.button === 0 && currentSquare.classList.contains('square') && !currentSquare.classList.contains('poop')) {
+
+      if (currentSquare.innerHTML === 'cat') {
+        currentSquare.classList.add('sink', 'poop');
+        var audio = {};
+        audio['meow'] = new Audio();
+        audio['meow'].src = "assets/Cat-meow-mp3.mp3";
+        audio['meow'].volume = 0.1;
+        audio['meow'].play();
+      } else {
+        currentSquare.classList.add('sink', 'show');
+
+        check(x, y);
+        safeCount--;
+        if (safeCount === 0) {
+          alert('YOU WIN!!!!');
+        }
+      }
     }
   }); 
 });
+
+var check = function(x, y) {
+  if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML === 'cat') {
+    return false;
+  } else if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML) {
+    return true;
+  } else {
+    if (y - 1 > 0 && !$('.' + (y - 1) + ' :nth-child(' + x + ')')[0].classList.contains('sink')) {
+      $('.' + (y - 1) + ' :nth-child(' + x + ')').addClass('sink');
+      $('.' + (y - 1) + ' :nth-child(' + x + ')').addClass('show');
+      check(x, y - 1);
+    }
+    if (y + 1 <= columns && !$('.' + (y + 1) + ' :nth-child(' + x + ')')[0].classList.contains('sink')) {
+      $('.' + (y + 1) + ' :nth-child(' + x + ')').addClass('sink');
+      $('.' + (y + 1) + ' :nth-child(' + x + ')').addClass('show');
+      check(x, y + 1);
+    }
+    if (x - 1 > 0 && !$('.' + y + ' :nth-child(' + (x - 1) + ')')[0].classList.contains('sink')) {
+      $('.' + y + ' :nth-child(' + (x - 1) + ')').addClass('sink');
+      $('.' + y + ' :nth-child(' + (x - 1) + ')').addClass('show');
+      check(x - 1, y);
+    }
+    if (x + 1 <= rows && !$('.' + y + ' :nth-child(' + (x + 1) + ')')[0].classList.contains('sink')) {
+      $('.' + y + ' :nth-child(' + (x + 1) + ')').addClass('sink');
+      $('.' + y + ' :nth-child(' + (x + 1) + ')').addClass('show');
+      check(x + 1, y);
+    }
+    return 1;
+  }
+};
