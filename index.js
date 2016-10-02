2c11f6a8e63c8457346eba9fddccb0e38b02f638
Hien Tran <aznketchup@yahoo.com>
2016-10-01 17:52:22 -0700

    Modularize and refactor code.
    -Seperate functionalities to make functions more modular.
    -Implement eachSquare, addCats, withRange, surroundingArea, countCats, blankEffect, checkSquare functions.
    -Refactor surroundingArea function to check 8 sides oppose to 4.
    -Download and source new font for remainder/timer.
    -Download and source new font for board.
    -Add color to numbers.
    -Add image to start button.
    -Initialize logic for click with both left and right mouse button.

--- index.js
+++ index.js
@@ -1,95 +1,154 @@
 var board = [];
 var rows = 9;
 var columns = 9;
-var catsRemaining = 10;
+var totalCats = 10;
 var catsLocation = {};
-var safeCount = rows * columns - catsRemaining;
+var safeCount = rows * columns - totalCats;
 
-for (var i = 0; i < rows; i++) {
-  var row = [];
-  for (var j = 0; j < columns; j++) {
-    row.push(0);
-  }
-  board.push(row);
-}
-
-while (catsRemaining > 0) {
-  var condition  = true;
-  while (condition) {
-    var x = Math.floor(Math.random() * rows) + 1;
-    var y = Math.floor(Math.random() * columns) + 1;
-    if (!catsLocation[x]) {
-      catsLocation[x] = [y];
-      board[x - 1][y - 1] = 'cat';
-      condition = false;
-    } else if (!catsLocation[x].includes(y)) {
-      catsLocation[x].push(y);
-      board[x - 1][y - 1] = 'cat';
-      condition = false;
+var eachSquare = function (callback, initialize) {
+  for (var i = 0; i < rows; i++) {
+    if (initialize) {
+      board[i] = initialize(i);
     }
-  }
-  catsRemaining--;
-}
-
-for (var i = 0; i < rows; i++) {
-  for (var j = 0; j < columns; j++) {
-    if (board[i][j] !== 'cat') {
-      var count = 0;
-      if(board[i - 1]) {
-        if (board[i - 1][j - 1] === 'cat') {
-          count++;
-        }
-        if (board[i - 1][j] === 'cat') {
-          count++;
-        }
-        if (board[i - 1][j + 1] === 'cat') {
-          count++;
-        }
-      }
-      if (board[i][j - 1] === 'cat') {
-        count++;
-      }
-      if (board[i][j + 1] === 'cat') {
-        count++;
-      }
-      if(board[i + 1]) {
-        if (board[i + 1][j - 1] === 'cat') {
-          count++;
-        }
-        if (board[i + 1][j] === 'cat') {
-          count++;
-        }
-        if (board[i + 1][j + 1] === 'cat') {
-          count++;
-        }
-      }
-      board[i][j] = count;
+    for (var j = 0; j < columns; j++) {
+      board[i][j] = callback(i, j);
     }
   }
 }
+
+var addCats = function(catsNeeded) {
+  while (catsNeeded > 0) {
+    var condition  = true;
+    while (condition) {
+      var x = Math.floor(Math.random() * rows);
+      var y = Math.floor(Math.random() * columns);
+      if (!catsLocation[x]) {
+        catsLocation[x] = [y];
+        board[x][y] = 'cat';
+        condition = false;
+      } else if (!catsLocation[x].includes(y)) {
+        catsLocation[x].push(y);
+        board[x][y] = 'cat';
+        condition = false;
+      }
+    }
+    catsNeeded--;  
+  }
+} 
+
+var withinRange = function (value, length) {
+  if (value >= 0 && value < length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var surroundingArea = function (x, y, callback, startsAtOne) {
+  var count = 0;
+  startsAtOne = startsAtOne ? 1 : 0;
+
+  for (var i = -1; i <= 1; i++) {
+    for (var j = -1; j <= 1; j++) {
+      if(withinRange(x + i - startsAtOne, rows) && withinRange(y + j - startsAtOne, columns)) {
+        count += callback(x + i, y + j);
+      }
+    }
+  }
+  return count;
+}
+
+var countCats = function (x, y) {
+  if (board[x][y] === 'cat') {
+    return 'cat';
+  } else {
+    return surroundingArea(x, y, function(i, j) {
+      if (board[i][j] === 'cat') {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+}
+
+var blankAffect = function (x, y) {
+  if (x <= rows && !$('.' + y + ' :nth-child(' + x + ')')[0].classList.contains('sink')) {
+    safeCount--;
+    $('.' + y + ' :nth-child(' + x + ')').addClass('sink').addClass('show');
+    checkSquare(x, y);
+  }
+}
+
+var checkSquare = function(x, y) {
+  if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML === 'cat') {
+    return false;
+  } else if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML) {
+    return true;
+  } else {
+    surroundingArea(x, y, blankAffect, true);
+    return 1;
+  }
+};
+
+//Initialize game
+eachSquare(function(){
+  return 0;
+}, function () {
+  return [];
+});
+addCats(totalCats);
+eachSquare(countCats);
 
 $(document).ready(function(){
-  for (var i = 0; i < rows; i++) {
-    for (var j = 0; j < columns; j++) {
-      if (board[i][j] !== 0) {
-        $('.' + (i + 1) + ' :nth-child(' + (j + 1) + ')').text(board[i][j]);
-      }
+  eachSquare(function (x, y){
+    var color = 'black';
+
+    switch (board[x][y]) {
+      case 1:
+        color = 'blue';
+        break;
+      case 2:
+        color = 'green';
+        break;
+      case 3:
+        color = 'red';
+        break;
+      case 4:
+        color = 'purple';
+        break;
+      case 5:
+        color = 'black';
+        break;
+      case 6:
+        color = 'maroon';
+        break;
+      case 7:
+        color = 'gray';
+        break;
+      case 8:
+        color = 'turquoise';
+        break;
+      default:
+        color = color
     }
-  }
+
+    if (board[x][y] !== 0) {
+      $('.' + (y + 1) + ' :nth-child(' + (x + 1) + ')').text(board[x][y]).css('color', color);
+    }
+  })
 
   //disable right click context menu
   document.oncontextmenu = function () {
     return false;
   };
 
-  $('.body').mousedown(function(event){
+  $('.board').mousedown(function(event){
     var currentSquare = event.target;
     var x = Number(currentSquare.classList[1]);
     var y = Number(currentSquare.parentElement.classList[1]);
 
-    if (event.button === 2 && currentSquare.classList.contains('square') && !currentSquare.classList.contains('sink')) {
-      event.target.classList.toggle('poop');
-    }
+    //Logic for left mouse click
     if (event.button === 0 && currentSquare.classList.contains('square') && !currentSquare.classList.contains('poop') && !currentSquare.classList.contains('sink')) {
       if (currentSquare.innerHTML === 'cat') {
         currentSquare.classList.add('sink', 'poop');
@@ -99,48 +158,26 @@ $(document).ready(function(){
         audio['meow'].volume = 0.1;
         audio['meow'].play();
       } else {
+        console.log(x, y);
         currentSquare.classList.add('sink', 'show');
-
-        check(x, y);
+        checkSquare(x, y);
         safeCount--;
         if (safeCount === 0) {
           alert('YOU WIN!!!!');
         }
       }
     }
+
+    //Logic for click both left and right mouse click
+    if (event.button === 1 && currentSquare.classList.contains('square') && currentSquare.classList.contains('sink')) {
+      if (surroundingArea(x, y, countCats, true) === 0) {
+        surroundingArea(x, y, checkSquare, true);
+      }
+    }
+
+    //Logic for right mouse click
+    if (event.button === 2 && currentSquare.classList.contains('square') && !currentSquare.classList.contains('sink')) {
+      event.target.classList.toggle('poop');
+    }
   }); 
 });
-
-var check = function(x, y) {
-  if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML === 'cat') {
-    return false;
-  } else if ($('.' + y + ' :nth-child(' + x + ')')[0].innerHTML) {
-    return true;
-  } else {
-    if (y - 1 > 0 && !$('.' + (y - 1) + ' :nth-child(' + x + ')')[0].classList.contains('sink')) {
-      safeCount--;
-      $('.' + (y - 1) + ' :nth-child(' + x + ')').addClass('sink');
-      $('.' + (y - 1) + ' :nth-child(' + x + ')').addClass('show');
-      check(x, y - 1);
-    }
-    if (y + 1 <= columns && !$('.' + (y + 1) + ' :nth-child(' + x + ')')[0].classList.contains('sink')) {
-      safeCount--;
-      $('.' + (y + 1) + ' :nth-child(' + x + ')').addClass('sink');
-      $('.' + (y + 1) + ' :nth-child(' + x + ')').addClass('show');
-      check(x, y + 1);
-    }
-    if (x - 1 > 0 && !$('.' + y + ' :nth-child(' + (x - 1) + ')')[0].classList.contains('sink')) {
-      safeCount--;
-      $('.' + y + ' :nth-child(' + (x - 1) + ')').addClass('sink');
-      $('.' + y + ' :nth-child(' + (x - 1) + ')').addClass('show');
-      check(x - 1, y);
-    }
-    if (x + 1 <= rows && !$('.' + y + ' :nth-child(' + (x + 1) + ')')[0].classList.contains('sink')) {
-      safeCount--;
-      $('.' + y + ' :nth-child(' + (x + 1) + ')').addClass('sink');
-      $('.' + y + ' :nth-child(' + (x + 1) + ')').addClass('show');
-      check(x + 1, y);
-    }
-    return 1;
-  }
-};
